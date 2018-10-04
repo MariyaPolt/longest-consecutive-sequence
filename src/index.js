c4ea1eb4e0a0272ce02a1b461eade4c096468d76
Mariya <mariya.poltoratskaya@gmail.com>
2018-10-04 16:54:09 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let set = new Set();
+  for (let i = 0; i < array.length; i++) {
+    set.add(array[i]);
+  }
+
+  let max = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    let count = 1;
+    let left = array[i] - 1;
+    let right = array[i] + 1;
+
+    while (set.delete(left)) {
+      count++;
+      left--;
+    }
+
+    while (set.delete(right)) {
+      count++;
+      right++;
+    }
+
+    max = Math.max(max, count);
+  }
+
+  return max;
 }
